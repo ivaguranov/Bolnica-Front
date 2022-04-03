d92e2a730308b3e95ddf64acfcb284e08f9c99ee
ivaguranov <iva.guranov@gmail.com>
2022-04-03 14:20:24 +0200

    added dividerAfter, changed active icon in SidebarLink, finished Sidebar component

--- src/components/SidebarLink/SidebarLink.js
+++ src/components/SidebarLink/SidebarLink.js
@@ -2,17 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
-const prop = ({ id, text, path, icon, isActive }) => (
+const prop = ({ id, text, path, icon, isActive, dividerAfter }) => (
   <li key={`prop-${id}`} className="nav-item">
-    <Link to={path} className={`nav-link ${isActive ? "active-link": ""}`}>
-      <i className={`${icon} customIcon`}></i>
+    <Link to={path} className={`nav-link ${isActive ? "active-link" : ""}`}>
+      <i className={`${icon} customIcon `}></i>
       <span>{text}</span>
     </Link>
+    {dividerAfter ? (
+      <div style={{ width: "100%", border: "1px solid black" }}></div>
+    ) : (
+      <></>
+    )}
   </li>
 );
 
 const SidebarLink = ({ props }) => {
-  return <ul>{props.map(prop)}</ul>;
+  return props.map(prop);
 };
 
 export default SidebarLink;
