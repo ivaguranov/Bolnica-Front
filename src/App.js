8aef0864f94611899f1048e964a8705a52588d1e
Dimitrije <dsejat@gmail.com>
2022-04-04 00:47:40 +0200

    Delete Demo login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DemoPage from "./pages/DemoPage/DemoPage";
-import DemoLogin from "./pages/DemoLogin/DemoLogin";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/demo-login" exact element={<DemoLogin />} />
 				<Route path="/" exact element={<DemoPage />} />
 				<Route path="/login" exact element={<LoginPage />} />
 			</Routes>
