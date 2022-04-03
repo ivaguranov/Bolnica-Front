d72801f9f798c543617e65aa61c78041765d0958
dragoslavmilutinovic <61113932+dragoslavmilutinovic@users.noreply.github.com>
2022-04-03 23:21:54 +0200

    added loginPage (#25)
    * added loginPage
    
    * Update src/App.js
    
    * Update src/App.js
    
    Co-authored-by: Dimitrije Sejat <42682353+Sheky1@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import DemoPage from "./components/DemoPage/DemoPage";
 import DemoLogin from "./components/DemoLogin/DemoLogin";
+import Login from   "./components/LoginComponent/Login"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" exact element={<DemoLogin />} />
+        <Route path="/demo-login" exact element={<DemoLogin />} />
         <Route path="/" exact element={<DemoPage />} />
+				<Route path="/login" exact element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
