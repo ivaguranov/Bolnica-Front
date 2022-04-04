6aae1482f5ba660274dd059d1d15afa28948ffcc
ivaguranov <iva.guranov@gmail.com>
2022-04-04 22:24:14 +0200

    finished Doctor homepage foundation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DemoPage from "./pages/DemoPage/DemoPage";
+import DoctorHomepage from "./pages/DoctorHomepage/DoctorHomepage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" exact element={<DemoPage />} />
-				<Route path="/login" exact element={<LoginPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<DoctorHomepage />} />
+        <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/demp-page" exact element={<DemoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/DoctorHomepage/DoctorHomepage.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Header from "../../components/Header/Header";
+import GeneralStats from "../../components/GeneralStats/GeneralStats";
+import { format } from "date-fns";
+
+const DoctorHomepage = () => {
+  const sidebarProps = [
+    {
+      id: 1,
+      text: "Pocetna",
+      path: "/",
+      icon: "fa-solid fa-chart-pie",
+      isActive: true,
+    },
+  ];
+
+  const headerProps = {
+    avatarUrl: "nikolaSlika 1.jpg",
+    welcomeMsg: "Dobro jutro",
+    userName: "Dr. Paun",
+    userTitle: "Kardiolog",
+  };
+
+  const generalStatsProps = [
+    {
+      image: "fa-solid fa-briefcase-medical",
+      text: "Zakazani pregledi",
+      number: "34",
+    },
+    {
+      image: "fa-solid fa-person-cane",
+      text: "Broj pacijenata",
+      number: "10",
+    },
+  ];
+
+  return (
+    <>
+      <div className="sidebar-link-container">
+        <Sidebar links={sidebarProps} />
+      </div>
+      <div style={{ marginLeft: "15%" }}>
+        <Header
+          avatarUrl={headerProps.avatarUrl}
+          welcomeMsg={headerProps.welcomeMsg}
+          userName={headerProps.userName}
+          userTitle={headerProps.userTitle}
+          day={format(new Date(), "d")}
+          date={format(new Date(), "d MMMM, yyyy")}
+        />
+
+        <div className="components">
+          <GeneralStats
+            image={generalStatsProps[0].image}
+            text={generalStatsProps[0].text}
+            number={generalStatsProps[0].number}
+          />
+          <GeneralStats
+            image={generalStatsProps[1].image}
+            text={generalStatsProps[1].text}
+            number={generalStatsProps[1].number}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DoctorHomepage;
