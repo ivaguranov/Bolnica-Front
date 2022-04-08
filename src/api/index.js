f565f9640dc61e7df0ee328b7831f6c21d489a38
Dimitrije <dsejat@gmail.com>
2022-04-08 18:12:43 +0200

    baseURL fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:9092" });
+const API = axios.create({ baseURL: "http://localhost:9092/api" });
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem("loggedUser")) {
