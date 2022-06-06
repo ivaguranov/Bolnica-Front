60b3ef1721cdf91dbdd914a8286b467ea7d04488
Dimitrije <dsejat@gmail.com>
2022-06-06 22:02:29 +0200

    Asd

--- src/pages/Doctor/DoctorHomepage/DoctorHomepage.js
+++ src/pages/Doctor/DoctorHomepage/DoctorHomepage.js
@@ -14,89 +14,89 @@ import { useNavigate } from "react-router";
 import { getSidebarLinks } from "../../../commons/sidebarLinks";
 
 const DoctorHomepage = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+	useEffect(() => {
+		const token = localStorage.getItem("token");
 
-    if (token) {
-      const doctor = JSON.parse(localStorage.getItem("loggedUser"));
-      dispatch(resetUser());
-      dispatch(getAppointments(doctor.LBZ));
-      dispatch(getPatients());
-    } else navigate("/login");
-  }, []);
+		if (token) {
+			const doctor = JSON.parse(localStorage.getItem("loggedUser"));
+			dispatch(resetUser());
+			dispatch(getAppointments(doctor.LBZ));
+			dispatch(getPatients());
+		} else navigate("/login");
+	}, []);
 
-  const appointments = useSelector((state) => state.appointments);
-  const patients = useSelector((state) => state.patients);
+	const appointments = useSelector((state) => state.appointments);
+	const patients = useSelector((state) => state.patients);
 
-  const headerProps = {
-    avatarUrl: "nikolaSlika 1.jpg",
-    welcomeMsg: "Dobro jutro",
-    userName: "Dr. Paun",
-    userTitle: "Kardiolog",
-  };
+	const headerProps = {
+		avatarUrl: "nikolaSlika 1.jpg",
+		welcomeMsg: "Dobro jutro",
+		userName: "Dr. Paun",
+		userTitle: "Kardiolog",
+	};
 
-  const generalStatsProps = [
-    {
-      image: <GiMedicalPack size="45px" />,
-      text: "Zakazani pregledi",
-      number: "34",
-    },
-    {
-      image: <FaUserInjured size="45px" />,
-      text: "Broj pacijenata",
-      number: "10",
-    },
-    {
-      image: <GiMedicalDrip size="45px" />,
-      text: "Operacije",
-      number: "10",
-    },
-  ];
+	const generalStatsProps = [
+		{
+			image: <GiMedicalPack size="45px" />,
+			text: "Zakazani pregledi",
+			number: "34",
+		},
+		{
+			image: <FaUserInjured size="45px" />,
+			text: "Broj pacijenata",
+			number: "10",
+		},
+		{
+			image: <GiMedicalDrip size="45px" />,
+			text: "Operacije",
+			number: "10",
+		},
+	];
 
-  return (
-    <>
-      <div className="sidebar-link-container">
-        <Sidebar links={getSidebarLinks("doctor", 1)} />
-      </div>
-      <div style={{ marginLeft: "15%" }}>
-        <Header
-          avatarUrl={headerProps.avatarUrl}
-          welcomeMsg={headerProps.welcomeMsg}
-          userName={headerProps.userName}
-          userTitle={headerProps.userTitle}
-          day={format(new Date(), "d")}
-          date={format(new Date(), "d MMMM, yyyy")}
-        />
+	return (
+		<>
+			<div className="sidebar-link-container">
+				<Sidebar links={getSidebarLinks("doctor", 1)} />
+			</div>
+			<div style={{ marginLeft: "15%" }}>
+				<Header
+					avatarUrl={headerProps.avatarUrl}
+					welcomeMsg={headerProps.welcomeMsg}
+					userName={headerProps.userName}
+					userTitle={headerProps.userTitle}
+					day={format(new Date(), "d")}
+					date={format(new Date(), "d MMMM, yyyy")}
+				/>
 
-        <div className="components">
-          <GeneralStats
-            image={generalStatsProps[0].image}
-            text={generalStatsProps[0].text}
-            number={generalStatsProps[0].number}
-          />
-          <GeneralStats
-            image={generalStatsProps[1].image}
-            text={generalStatsProps[1].text}
-            number={generalStatsProps[1].number}
-          />
-          <GeneralStats
-            image={generalStatsProps[2].image}
-            text={generalStatsProps[2].text}
-            number={generalStatsProps[2].number}
-          />
-        </div>
-        {appointments && patients.length > 0 && (
-          <ScheduledAppointments
-            appointments={appointments}
-            patients={patients}
-          />
-        )}
-      </div>
-    </>
-  );
+				<div className="components">
+					<GeneralStats
+						image={generalStatsProps[0].image}
+						text={generalStatsProps[0].text}
+						number={generalStatsProps[0].number}
+					/>
+					<GeneralStats
+						image={generalStatsProps[1].image}
+						text={generalStatsProps[1].text}
+						number={generalStatsProps[1].number}
+					/>
+					<GeneralStats
+						image={generalStatsProps[2].image}
+						text={generalStatsProps[2].text}
+						number={generalStatsProps[2].number}
+					/>
+				</div>
+				{appointments && patients.length > 0 && (
+					<ScheduledAppointments
+						appointments={appointments}
+						patients={patients}
+					/>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default DoctorHomepage;
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -3,37 +3,38 @@ import * as api from "../../api/index.js";
 import jwt from "jwt-decode";
 
 export const login = (formData, navigate) => async (dispatch) => {
-  try {
-    const { data } = await api.login(formData);
-    const user = jwt(data);
-    const roles = user.roles.split(",");
-    localStorage.setItem("token", data);
-    localStorage.setItem("loggedUser", JSON.stringify(user));
-    dispatch({ type: LOGIN, payload: user });
-    if (roles.includes("ROLE_ADMIN")) navigate("/admin");
-    else if (roles.includes("ROLE_DR_SPEC_POV")) navigate("/");
-    else navigate("/nurse");
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const { data } = await api.login(formData);
+		const user = jwt(data);
+		const roles = user.roles.split(",");
+		localStorage.setItem("token", data);
+		localStorage.setItem("loggedUser", JSON.stringify(user));
+		dispatch({ type: LOGIN, payload: user });
+		if (roles.includes("ROLE_ADMIN")) navigate("/admin");
+		else if (roles.includes("ROLE_DR_SPEC_POV")) navigate("/");
+		else navigate("/nurse");
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const logout = (navigate) => async (dispatch) => {
-  try {
-    localStorage.removeItem("token");
-    dispatch({ type: LOGOUT });
-    navigate("/login");
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		localStorage.removeItem("token");
+		localStorage.removeItem("loggedUser");
+		dispatch({ type: LOGOUT });
+		navigate("/login");
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const resetUser = () => async (dispatch) => {
-  try {
-    const data = localStorage.getItem("token");
-    const user = jwt(data);
-    dispatch({ type: LOGIN, payload: user });
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const data = localStorage.getItem("token");
+		const user = jwt(data);
+		dispatch({ type: LOGIN, payload: user });
+	} catch (error) {
+		console.log(error);
+	}
 };
